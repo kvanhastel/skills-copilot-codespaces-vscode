23ff756f00a45ec8f0fce40a579dc78465006475
kvanhastel <38067868+kvanhastel@users.noreply.github.com>
2024-05-26 14:14:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,55 +2,52 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var path = require('path');
-var comments = [];
-var server = http.createServer(function(req, res){
-    var parseUrl = url.parse(req.url, true);
-    var pathName = parseUrl.pathname;
-    if(pathName === '/'){
-        fs.readFile('./index.html', function(err, data){
-            if(err){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end('404 Not Found');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    }else if(pathName === '/post'){
-        fs.readFile('./post.html', function(err, data){
-            if(err){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end('404 Not Found');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    }else if(pathName === '/comment'){
-        var comment = parseUrl.query;
-        comments.push(comment);
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(JSON.stringify(comments));
-    }else if(pathName === '/getcomment'){
-        var str = JSON.stringify(comments);
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(str);
-    }else{
-        var realPath = path.join('./', pathName);
-        fs.exists(realPath, function(exists){
-            if(!exists){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end('404 Not Found');
-            }else{
-                fs.readFile(realPath, function(err, data){
-                    if(err){
-                        res.writeHead(500, {'Content-Type': 'text/html'});
-                        res.end('500 Server Error');
-                    }
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(data);
-                });
-            }
-        });
-    }
-});
-server.listen(3000, ');)))');
+var querystring = require('querystring');
+
+//create server
+http.createServer(function(req, res){
+	//parse the request
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var pathname = url_parts.pathname;
+
+	//respond to the request
+	if(pathname == '/'){
+		//read the file
+		fs.readFile('comments.html', function(err, data){
+			//send the file
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(data);
+		});
+	}
+	else if(pathname == '/comments'){
+		//read the file
+		fs.readFile('comments.txt', 'utf8', function(err, data){
+			//send the file
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end(data);
+		});
+	}
+	else if(pathname == '/addComment'){
+		//read the file
+		fs.readFile('comments.txt', 'utf8', function(err, data){
+			//parse the file
+			var comments = JSON.parse(data);
+			//add a new comment
+			comments.push(query);
+			//write the file
+			fs.writeFile('comments.txt', JSON.stringify(comments), function(err){
+				//send the file
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				res.end('{"status": "success"}');
+			});
+		});
+	}
+	else{
+		//send a 404
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.end('404: Page not found');
+	}
+}).listen(3000);
+
+console.log('Server running on port 3000');
