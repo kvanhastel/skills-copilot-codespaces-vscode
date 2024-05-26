4ae655e4db75115e0395b3c3c4f34f1f97adca24
kvanhastel <38067868+kvanhastel@users.noreply.github.com>
2024-05-26 14:11:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,71 +1,56 @@
 //create web server
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const querystring = require('querystring');
-const path = require('path');
-const comments = [];
-http.createServer((req, res) => {
-    const parseUrl = url.parse(req.url);
-    const pathname = parseUrl.pathname;
-    if (pathname === '/') {
-        fs.readFile('./index.html', 'utf8', (err, data) => {
-            if (err) {
-                res.statusCode = 500;
-                res.end('Internal Server Error');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = [];
+var server = http.createServer(function(req, res){
+    var parseUrl = url.parse(req.url, true);
+    var pathName = parseUrl.pathname;
+    if(pathName === '/'){
+        fs.readFile('./index.html', function(err, data){
+            if(err){
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('404 Not Found');
             }
-            res.setHeader('Content-Type', 'text/html');
+            res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
         });
-    } else if (pathname === '/comment') {
-        if (req.method === 'POST') {
-            let body = '';
-            req.on('data', (chunk) => {
-                body += chunk;
-            });
-            req.on('end', () => {
-                const query = querystring.parse(body);
-                comments.push(query.comment);
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                res.end();
-            });
-        } else if (req.method === 'GET') {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(comments));
-        } else {
-            res.statusCode = 404;
-            res.end('Not Found');
-        }
-    } else {
-        const ext = path.parse(pathname).ext;
-        fs.readFile(`.${pathname}`, 'utf8', (err, data) => {
-            if (err) {
-                res.statusCode = 404;
-                res.end('Not Found');
+    }else if(pathName === '/post'){
+        fs.readFile('./post.html', function(err, data){
+            if(err){
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('404 Not Found');
             }
-            const contentType = {
-                '.html': 'text/html',
-                '.css': 'text/css',
-                '.js': 'text/javascript',
-            };
-            res.setHeader('Content-Type', contentType[ext]);
+            res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
+        });
+    }else if(pathName === '/comment'){
+        var comment = parseUrl.query;
+        comments.push(comment);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(JSON.stringify(comments));
+    }else if(pathName === '/getcomment'){
+        var str = JSON.stringify(comments);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(str);
+    }else{
+        var realPath = path.join('./', pathName);
+        fs.exists(realPath, function(exists){
+            if(!exists){
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('404 Not Found');
+            }else{
+                fs.readFile(realPath, function(err, data){
+                    if(err){
+                        res.writeHead(500, {'Content-Type': 'text/html'});
+                        res.end('500 Server Error');
+                    }
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(data);
+                });
+            }
         });
     }
-}).listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
 });
-// Path: index.html
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Comments</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <h1>Comments</h1>
-    <form action="/comment" method="post">
-        <input type="text" name="comment" placeholder="Enter your comment">
-        <input type="
+server.listen(3000, ');)))');
